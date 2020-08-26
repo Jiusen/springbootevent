e390b3238d48f24824442cd10a7a46c8208090d4
Guo Jiusen <2434732570@qq.com>
2020-08-26 17:25:13 +0800

    开始springboot-web项目的开发

---
+++ springsootweb/src/main/java/com/jiusen/springsootweb/SpringsootwebApplication.java
@@ -0,0 +1,15 @@
+package com.jiusen.springsootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsootwebApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringsootwebApplication.class, args);
+    }
+
+}
---
+++ springsootweb/src/test/java/com/jiusen/springsootweb/SpringsootwebApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jiusen.springsootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsootwebApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
