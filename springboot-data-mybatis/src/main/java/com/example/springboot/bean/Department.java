d39a97f070b279a2ba5d5f0845626d56beacd449
Guo Jiusen <2434732570@qq.com>
2020-10-14 11:17:26 +0800

    基础环境搭建（数据源druid的配置、表的创建）

---
+++ springboot-data-mybatis/src/main/java/com/example/springboot/bean/Department.java
@@ -0,0 +1,28 @@
+package com.example.springboot.bean;
+
+/**
+ * @Author Jiusen Guo
+ * @Date 2020/10/14 11:13
+ * @Version 1.0
+ */
+public class Department {
+
+    private Integer id;
+    private String departmentName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
---
+++ springboot-data-mybatis/src/main/java/com/example/springboot/bean/Employee.java
@@ -0,0 +1,55 @@
+package com.example.springboot.bean;
+
+/**
+ * @Author Jiusen Guo
+ * @Date 2020/10/14 11:11
+ * @Version 1.0
+ */
+public class Employee {
+
+    private Integer id;
+    private String lastName;
+    private Integer gender;
+    private String email;
+    private Integer dId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+}
